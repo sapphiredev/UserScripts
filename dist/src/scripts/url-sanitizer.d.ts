1396bf5f63ce7e52ed8a20c9b22104515306e7d4
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-19 12:42:56 +0000

    travis-ci build: 31

---
+++ dist/src/scripts/url-sanitizer.d.ts
@@ -0,0 +1,18 @@
+declare enum WebsiteKeys {
+    TWITTER = "twitter",
+    PIXIV = "pixiv",
+    PIXIV_FANBOX = "pixiv_fanbox",
+    MELONBOOKS = "melonbooks",
+    TORANOANA = "toranoana"
+}
+declare type RegularExpressions = {
+    [T in WebsiteKeys]: RegExp;
+};
+interface Query {
+    [key: string]: string;
+}
+declare function parseQuery(text: string): Query;
+declare function stringifyQuery(query: Query): string;
+declare function processQuery(key: WebsiteKeys, text: string): string | undefined;
+declare function getSanitizedURL(key: WebsiteKeys, match: RegExpMatchArray): string;
+declare const regularExpressions: RegularExpressions;
